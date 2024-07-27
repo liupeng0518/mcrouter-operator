16a6163ec4feb0595187b3150e0d90c18c8690b9
Samuel Liu <liupeng0518@gmail.com>
2024-07-27 12:27:26 +0800

    init proxy and server

--- cmd/main.go
+++ cmd/main.go
@@ -129,6 +129,19 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "MemcachedProxy")
 		os.Exit(1)
 	}
+	if err = (&controller.MemcachedReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Memcached")
+		os.Exit(1)
+	}
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&cachev1alpha1.Memcached{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Memcached")
+			os.Exit(1)
+		}
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
